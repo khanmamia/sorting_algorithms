f1bc3813ea4f84addd9e2559d8560e7130d355cb
Arafat Mamia <arafmamia@gmail.com>
2022-01-08 23:54:31 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts bubbly
+ * @array: array to sort
+ * @size: size
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i ,j, newsize = size;
+	int tmp, swap;
+
+	if (array == NULL || size < 2)
+		return;
+
+        for(i = 0;i< newsize-1;i++){
+	
+		swap = 0;
+		for (j = 0; j < newsize - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+		if (!swap)
+			break;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-**A repository containing various sorting alogorithms implemented in C along with their time and space complexities analysis**
+# General
+* At least four different sorting algorithms
+* What is the Big O notation, and how to evaluate the time complexity of an algorithm
+* How to select the best sorting algorithm for a given input
+* What is a stable sorting algorithm
+# About
+#A repository containing
+* Sorting Algorithm
+* Time Complexity and Big -oh notation
+* Stability
+# Requirements
+* Ubuntu 14.04
+* gcc 4.8.4
