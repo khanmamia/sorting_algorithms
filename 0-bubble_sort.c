a334d383f5f36422da80df08787108e089f6a52d
Arafat Mamia <arafmamia@gmail.com>
2022-01-09 00:35:17 -0800

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,25 +7,26 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i ,j, newsize = size;
+	size_t i, j, newsize = size;
 	int tmp, swap;
 
-	if (array == NULL || size < 2)
-		return;
+if (array == NULL || size < 2)
+	return;
 
-        for(i = 0;i< newsize-1;i++){
-	
-		swap = 0;
-		for (j = 0; j < newsize - i; j++)
+for (i = 0; i < newsize - 1; i++)
+{
+	swap = 0;
+
+	for (j = 0; j < newsize - i; j++)
+	{
+		if (array[j] > array[j + 1])
 		{
-			if (array[j] > array[j + 1])
-			{
-				tmp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = tmp;
-				print_array(array, size);
-				swap = 1;
-			}
+			tmp = array[j + 1];
+			array[j + 1] = array[j];
+			array[j] = tmp;
+			print_array(array, size);
+			swap = 1;
+		}
 		}
 		if (!swap)
 			break;
